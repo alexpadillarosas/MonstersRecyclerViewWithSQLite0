b6da9104a617fa0daab4f70c79820afa8e0dbd44
kraken <alexpadillarosas@gmail.com>
2020-04-27 16:33:33 +1000

    initial commit

---
+++ app/src/main/java/com/blueradix/android/monstersrecyclerviewwithsqlite/MainActivity.java
@@ -0,0 +1,80 @@
+package com.blueradix.android.monstersrecyclerviewwithsqlite;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        /**
+         * TODO: create a recycler view in content_main.xml file
+         */
+
+        /**
+         * TODO: create a recycler view item call it recycler_item_view.xml (do not forget to use constraint layout)
+         */
+
+        /**
+         * TODO: Implement the RecyclerView in onCreate
+         */
+
+        /**
+         * TODO: call the database, to get allMonsters
+         */
+
+        /**
+         * TODO: create a RecyclerViewAdapter and pass the data
+         */
+
+        /**
+         * TODO: set the adapter to the RecyclerView
+         */
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
